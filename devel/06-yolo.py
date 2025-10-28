f7b25bf04318389e7fc8022bafb4b23de9f652b6
LIS generic user <office@lis.tu-berlin.de>
2025-10-28 20:09:52 +0100

    yolo works

--- devel/06-yolo.py
+++ devel/06-yolo.py
@@ -2,6 +2,8 @@ import torch
 import robotic as ry
 import hackathon_distillation as hack
 import cv2
+import numpy as np
+from ultralytics import YOLO
 
 class Robot:
     def __init__(self, real=False):
@@ -10,8 +12,9 @@ class Robot:
         self.bot = ry.BotOp(C=self.S.C, useRealRobot=real)
         
     def run(self):
-        
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+        #model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        model = YOLO("yolo11n.pt", task='detect')
 
         rgb, depth = self.bot.getImageAndDepth('cameraWrist')
         D = hack.DataPlayer(rgb, depth)
@@ -22,7 +25,21 @@ class Robot:
                 break
 
             rgb, depth = self.bot.getImageAndDepth('cameraWrist')
+
+            depth_vis = cv2.normalize(depth, None, 0, 255, cv2.NORM_MINMAX)
+            depth_vis = depth_vis.astype(np.uint8)
+
+            cv2.imwrite('depth.png', depth_vis)
+
+            # Dummy
+            imgs = ['depth.png']
+            results = model(imgs)
+
+            results[0].plot()
+
             D.update(rgb, depth)
+
+            break
 
 if __name__ == "__main__":
 
